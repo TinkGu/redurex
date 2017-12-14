ad34f7ed6509a6860bda76c756b6b0c2551753a8
tinkgu <tlwrite@126.com>
2017-12-14 14:44:32 +0800

    [fix] add polyfill: regeneratorRuntime

--- rollup.config.js
+++ rollup.config.js
@@ -1,42 +1,38 @@
 import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
 import replace from 'rollup-plugin-replace'
 import uglify from 'rollup-plugin-uglify'
 
 const env = process.env.NODE_ENV
-const _exports = 'named'
+
 const config = {
+    name: 'Redurex',
     input: 'src/index.js',
-    plugins: [],
+    output: {
+        format: 'umd',
+        exports: 'named',
+    },
+    plugins: [
+        nodeResolve({
+            jsnext: true
+        }),
+        commonjs({
+            include: 'node_modules/**',
+        }),
+        babel({
+            exclude: 'node_modules/**',
+            runtimeHelpers: true
+        }),
+        replace({
+            'process.env.NODE_ENV': JSON.stringify(env)
+        })
+    ],
     external: ['redux', 'redux-saga'],
     globals: {
         redux: 'Redux',
         'redux-saga': 'ReduxSaga',
     }
-}
-
-if (env === 'es' || env === 'cjs') {
-    config.output = { format: env, exports: _exports }
-    config.plugins.push(babel({
-        plugins: ['external-helpers']
-    }))
-}
-
-if (env === 'development' || env === 'production') {
-    config.output = { format: 'umd', exports: _exports }
-    config.name = 'Redurex'
-    config.plugins.push(
-        nodeResolve({
-            jsnext: true
-        }),
-        babel({
-            exclude: 'node_modules/**',
-            plugins: ['external-helpers']
-        }),
-        replace({
-            'process.env.NODE_ENV': JSON.stringify(env)
-        })
-    )
 }
 
 if (env === 'production') {
